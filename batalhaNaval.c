2584f2db0c7be6bf9a9feae42686848fc3019649
Lucas da Silva <133595928+LucazsSilva@users.noreply.github.com>
2025-06-03 22:47:01 +0000

    desafio aventureiro  - diagonais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
+
 int main() {
     int tabuleiro[10][10];
+    int soma = 0;
 
     // Inicializa o tabuleiro com 0 (água)
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
+            tabuleiro[i][j] = soma;
         }
     }
 
-    // Posiciona navio horizontal 
+    // Posiciona navio horizontal
     for (int i = 0; i < 3; i++) {
         tabuleiro[1][3 + i] = 3;
     }
 
-    // Posiciona navio vertical 
+    // Posiciona navio vertical
     for (int i = 0; i < 3; i++) {
         tabuleiro[5 + i][7] = 3;
+    }
+    printf("\n");
+
+    // Posiciona navio diagonal principal
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][i - 2] = 3;
+    }
+
+    // Posiciona navio diagonal inversa
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][i - 6] = 3;
     }
 
     // Exibe o tabuleiro
