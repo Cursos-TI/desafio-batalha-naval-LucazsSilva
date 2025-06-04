c1663991c797d8a0f0c63daf51800a45c6b0a649
Lucas da Silva <133595928+LucazsSilva@users.noreply.github.com>
2025-06-04 22:46:41 +0000

    desafio - mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,8 +4,11 @@
 int main() {
     int tabuleiro[10][10];
     int soma = 0;
+    int cone[5][5];
+    int cruz[5][5];
+    int octaedro[5][5];
 
-    // Inicializa o tabuleiro com 0 (água)
+    // Inicializa o tabuleiro com 0
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             tabuleiro[i][j] = soma;
@@ -19,9 +22,11 @@ int main() {
 
     // Posiciona navio vertical
     for (int i = 0; i < 3; i++) {
-        tabuleiro[5 + i][7] = 3;
+        for (int j = 0; j < 3; j++)
+        {
+            tabuleiro[5 + i][j] = 3;
+        }   
     }
-    printf("\n");
 
 
     // diagonal principal
@@ -42,9 +47,39 @@ int main() {
         }
     }
 
+    // Matriz cone 
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j >= 2 - i && j <= 2 + i) {
+                tabuleiro[i][j] = 1;
+            } else {
+                tabuleiro[i][j] = 0;
+            }
+        }
+    }
 
+    // Matriz cruz
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 2 || j == 2) {
+                tabuleiro[3 + i][5 + j] = 3;
+            }
+        }
+    }
 
+    // Matriz octaedro
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            int dist_i = i - 2;
+            if (dist_i < 0) dist_i = -dist_i;
+            int dist_j = j - 2;
+            if (dist_j < 0) dist_j = -dist_j;
 
+            if (dist_i + dist_j <= 2) {
+                tabuleiro[5 + i][j] = 5; 
+            }
+        }
+    }
     // Exibe o tabuleiro
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
